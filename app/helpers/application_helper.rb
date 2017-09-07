1891df8c1dac5a05817e28abe316ace928f11914
mysmallprojects <lodhaarihant@gmail.com>
2017-09-07 12:30:50 +0000

    add image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+def gravatar_for(user, options={size: 80})
+  
+# create the md5 hash
+gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+size = options[:size]
+# compile URL which can be used in <img src="RIGHT_HERE"...
+gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+image_tag(gravatar_url, alt: user.username, class: "img-circle")
+
+end
 end
